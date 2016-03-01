46ea207b87c541b0de3c283f8b633e1589f4da4c
Harry Mumford-Turner <harrymt@me.com>
2016-03-01 18:16:16 +0000

    Added example endpoint

--- API.class.php
+++ API.class.php
@@ -29,7 +29,7 @@ abstract class API
      * Property: file
      * Stores the input of the PUT request
      */
-     protected $file = Null;
+    protected $file = Null;
 
     /**
      * Constructor: __construct
@@ -42,6 +42,7 @@ abstract class API
 
         $this->args = explode('/', rtrim($request, '/'));
         $this->endpoint = array_shift($this->args);
+
         if (array_key_exists(0, $this->args) && !is_numeric($this->args[0])) {
             $this->verb = array_shift($this->args);
         }
@@ -73,6 +74,41 @@ abstract class API
             $this->_response('Invalid Method', 405);
             break;
         }
+    }
+
+    public function processAPI() {
+        if (method_exists($this, $this->endpoint)) {
+            return $this->_response($this->{$this->endpoint}($this->args));
+        }
+
+        return $this->_response("No Endpoint: $this->endpoint" , 404);
+    }
+
+    private function _response($data, $status = 200) {
+        header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
+        return json_encode($data);
+    }
+
+    private function _cleanInputs($data) {
+        $clean_input = Array();
+        if (is_array($data)) {
+            foreach ($data as $k => $v) {
+                $clean_input[$k] = $this->_cleanInputs($v);
+            }
+        } else {
+            $clean_input = trim(strip_tags($data));
+        }
+        return $clean_input;
+    }
+
+    private function _requestStatus($code) {
+        $status = array(
+            200 => 'OK',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            500 => 'Internal Server Error',
+        );
+        return ($status[$code])?$status[$code]:$status[500];
     }
 }
 ?>
