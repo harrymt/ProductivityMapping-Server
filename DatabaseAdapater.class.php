3715984243c371e90e1018acf247e9e370c1a01d
Harry Mumford-Turner <harrymt@me.com>
2016-03-02 22:18:53 +0000

    Added apps and zones endpoint
    - Return zone details on /zones/location

--- DatabaseAdapater.class.php
+++ DatabaseAdapater.class.php
@@ -6,6 +6,27 @@ class DatabaseAdapater
     public function __construct() {
     }
 
+    // Utility function to create dummy data
+    private function makeArr($number, $str) {
+        $i = 0; $stack = array();
+        while ($i < $number) {
+            array_push($stack, $str);
+            $i++;
+        };
+
+        return $stack;
+    }
+
+    // Utility function to create dummy data
+    private function makeZoneObj() {
+        return array(
+            "name" => "my zone",
+            "radius" => 47.1,
+            "lat" => 52.123123,
+            "lng" => 52.123123
+        );
+    }
+
     /**
      * Gets the top $number of popular keywords.
      *
@@ -13,13 +34,7 @@ class DatabaseAdapater
      * @return String array Keywords.
      */
     public function getKeywords($number) {
-        $i = 0; $stack = array();
-        while ($i < $number) {
-            array_push($stack, "a_keyword");
-            $i++;
-        };
-
-        return $stack;
+        return $this->makeArr($number, "a_keyword");
     }
 
     /**
@@ -32,15 +47,49 @@ class DatabaseAdapater
      * @return String array Keywords.
      */
     public function getKeywordsNearLocation($number, $lat, $lng, $radius) {
-        $i = 0; $stack = array();
-        while ($i < $number) {
-            array_push($stack, "keyword_near_(" . $lat . "," . $lng . ")_with_radius_" . $radius);
-            $i++;
-        };
-
-        return $stack;
+        return $this->makeArr($number, "keyword_near_(" . $lat . "," . $lng . ")_with_radius_" . $radius);
     }
 
+
+    /**
+     * Gets the top $number of popular keywords.
+     *
+     * @param  int $number Number of keywords to get.
+     * @return String array Keywords.
+     */
+    public function getApps($number) {
+        return $this->makeArr($number, "a_keyword");
+    }
+
+    /**
+     * Gets the top $number of popular apps near a location.
+     *
+     * @param  int $number  Number of apps to get.
+     * @param  float $lat    Latitude of location
+     * @param  float $lng    Longitude of location
+     * @param  float $radius Radius from location.
+     * @return String array Apps.
+     */
+    public function getAppsNearLocation($number, $lat, $lng, $radius) {
+        return $this->makeArr($number, "keyword_near_(" . $lat . "," . $lng . ")_with_radius_" . $radius);
+    }
+
+    /**
+     * Gets the top $number of zones near a location.
+     *
+     * @param  int $number  Number of zones to get.
+     * @param  float $lat    Latitude of location
+     * @param  float $lng    Longitude of location
+     * @param  float $radius Radius from location.
+     * @return String array Zone.
+     */
+    public function getZonesNearLocation($number, $lat, $lng, $radius) {
+        return array(
+            $this->makeZoneObj(),
+            $this->makeZoneObj(),
+            $this->makeZoneObj()
+        );
+    }
  }
 
 ?>
--- MyAPI.class.php
+++ MyAPI.class.php
@@ -3,13 +3,26 @@
 require_once 'API.class.php';
 require_once 'DatabaseAdapater.class.php';
 
+/**
+ *
+ * GET /status/
+ * GET /status/date/
+ * GET /status/time/
+ *
+ * GET /keywords/number/
+ * GET /keywords/number/lat/lng/radius/
+ *
+ * GET /apps/number/
+ * GET /apps/number/lat/lng/radius/
+ *
+ * GET /zones/number/lat/lng/radius/
+ *
+*/
 class MyAPI extends API
 {
     public function __construct($query_string, $api_endpoint) {
         parent::__construct($query_string, $api_endpoint);
     }
-
-
 
     /**
      * Gets the status of the API, returns OK or BAD.
@@ -49,10 +62,8 @@ class MyAPI extends API
         }
     }
 
-
-
     /**
-     * Get popular keywords
+     * Get popular keywords.
      *
      * GET /keywords/number/
      * GET /keywords/number/lat/lng/radius/
@@ -95,6 +106,87 @@ class MyAPI extends API
             return new Response_Wrapper("Only accepts GET requests", 405);
         }
     }
- }
+
+    /**
+     * Get popular apps.
+     *
+     * GET /apps/number/
+     * GET /apps/number/lat/lng/radius/
+     *
+     */
+    public function apps($arguments) {
+        if ($this->method == 'GET') {
+
+            //
+            // /apps/number/lat/lng/radius/
+            //
+            if($arguments >= 4 && is_numeric($arguments[0])
+                && is_numeric($arguments[1]) // lat
+                && is_numeric($arguments[2]) // lng
+                && is_numeric($arguments[3]) // radius
+                ) {
+
+                $adapter = new DatabaseAdapater();
+                $number = $this->get_numeric($arguments[0]);
+                $lat = $this->get_numeric($arguments[1]);
+                $lng = $this->get_numeric($arguments[2]);
+                $radius = $this->get_numeric($arguments[3]);
+                return new Response_Wrapper($adapter->getAppsNearLocation($number, $lat, $lng, $radius));
+            }
+
+            //
+            // /apps/number/
+            //
+            if($arguments >= 1 && is_numeric($arguments[0])) {
+                $adapter = new DatabaseAdapater();
+                $number = $this->get_numeric($arguments[0]);
+                return new Response_Wrapper($adapter->getApps($number));
+            }
+
+            //
+            // /apps/
+            //
+            return new Response_Wrapper("Please provide a number /apps/number/", 405);
+        } else {
+            return new Response_Wrapper("Only accepts GET requests", 405);
+        }
+    }
+
+    /**
+     * Get zones near a location.
+     *
+     * GET /zones/number/lat/lng/radius/
+     *
+     */
+    public function zones($arguments) {
+        if ($this->method == 'GET') {
+
+            //
+            // /zones/number/lat/lng/radius/
+            //
+            if($arguments >= 4 && is_numeric($arguments[0])
+                && is_numeric($arguments[1]) // lat
+                && is_numeric($arguments[2]) // lng
+                && is_numeric($arguments[3]) // radius
+                ) {
+
+                $adapter = new DatabaseAdapater();
+                $number = $this->get_numeric($arguments[0]);
+                $lat = $this->get_numeric($arguments[1]);
+                $lng = $this->get_numeric($arguments[2]);
+                $radius = $this->get_numeric($arguments[3]);
+                return new Response_Wrapper($adapter->getZonesNearLocation($number, $lat, $lng, $radius));
+            }
+
+
+            //
+            // /zones/
+            //
+            return new Response_Wrapper("Please provide a number and location /zones/number/lat/lng/radius/", 405);
+        } else {
+            return new Response_Wrapper("Only accepts GET requests", 405);
+        }
+    }
+}
 
 ?>
