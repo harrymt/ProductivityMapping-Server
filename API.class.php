8c7efc8ac2cc12cf266d9a19aee3561ea8174ef6
Harry Mumford-Turner <harrymt@me.com>
2016-03-09 13:25:30 +0000

    Fixed a typo to allow CORS

--- API.class.php
+++ API.class.php
@@ -45,7 +45,7 @@ abstract class API
             throw new Exception("Wrong api key");
         }
 
-        header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: *");
         header("Content-Type: application/json");
 
