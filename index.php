6f27ca1edc3c78578482c6132c2c1650b6ce5855
Harry Mumford-Turner <harrymt@me.com>
2016-03-09 19:12:18 +0000

    Tidied basic front end endpoints

--- index.php
+++ index.php
@@ -56,19 +56,34 @@
         $server_time = requestGETFromAPI('/status/time'); date_default_timezone_set('GMT'); $st = new DateTime("@$server_time");
         $server_time_formatted = $st->format('H:i:s');
 
+        $keywords = requestGETFromAPI('/keywords/3');
+        $keywords_str = ""; foreach($keywords as $key => $value) { $keywords_str .= "$key : $value<br>"; }
+
+        $apps = requestGETFromAPI('/apps/3');
+        $apps_str = ""; foreach($apps as $key => $value) { $apps_str .= "$key : $value<br>"; }
+
+        $zone_lat = "51.2"; $zone_lng = "-1.19"; $zone_radius = "10";
+        $zones = requestGETFromAPI("/zones/3/$zone_lat/$zone_lng/$zone_radius");
+        foreach ($zones as $zone) {
+            foreach($zone as $key => $value) { $zones_str .= "$key : $value<br>"; } $zones_str .= "<br>";
+        }
+
     ?>
 
     <h1>Study Mapping</h1>
     <p><?= $server_status; ?> at <?= $server_date . ' ' . $server_time_formatted; ?></p>
 
     <h2>Keywords</h2>
-    <code><?= var_dump(requestGETFromAPI('/keywords/3')); ?></code>
+    <p>Top 3 most used Keywords</p>
+    <code><?= $keywords_str ?></code>
 
     <h2>Apps</h2>
-    <code><?= var_dump(requestGETFromAPI('/apps/3')); ?></code>
+    <p>Top 3 most popular blocked Apps</p>
+    <code><?= $apps_str ?></code>
 
     <h2>Zones</h2>
-    <code><?= var_dump(requestGETFromAPI('/zones/3/1/1/100000000')); ?></code>
+    <p>3 nearest Zones to <?= $zone_lat ?>,<?= $zone_lng ?> in radius of <?= $zone_radius ?> meters</p>
+    <code><?= $zones_str ?></code>
 
 </body>
 </html>
