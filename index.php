e8e077067d6e03dd6fbdc966c4213c80e74b1b7f
Harry Mumford-Turner <harrymt@me.com>
2016-03-09 13:41:28 +0000

    Move to CURL requests, rather than file get contents

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <?php
 
-    require_once 'MyAPI.class.php';
     require_once '../Environment_variable.class.php';
 
     $api_key = "?apikey=" . Environment_variable::$API_KEY;
     $api_url = Environment_variable::$SERVER_URL;
+
+    /**
+     * Perform a GET request to the API.
+     *
+     * @param $url /v1/status/time
+     * @param bool $data array("param" => "value") ==> index.php?param=value
+     * @return mixed JSON output.
+     */
+    function requestGETFromAPI($url, $data = false)
+    {
+        $curl = curl_init();
+
+        if ($data) {
+            $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
+    }
 
 ?>
 
@@ -32,7 +56,7 @@
     <h1>Study Mapping</h1>
     <p>The status of the API is <span class="status status-good">good</span></p>
     <p><?php
-        $response = file_get_contents($api_url . '/status/date' . $api_key);
+        $response = requestGETFromAPI($api_url . '/status/date' . $api_key);
         echo json_decode($response)[0];
     ?></p>
 
